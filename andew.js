2e833e56f518e3e6e8629748f9f2f8a7191374be
GrayPLe <60240448+GrayPLe@users.noreply.github.com>
2020-02-25 14:55:06 -0800

    Update andew.js

--- andew.js
+++ andew.js
@@ -1,14 +1,17 @@
 function send(){
 	var pullInput = " " + document.getElementById('i').value;
 	userInput.push(pullInput);
-	// document.getElementById("chatbox").innerHTML = userInput;
+	document.getElementById("chatbox").innerHTML = userInput;
 	document.getElementById("i").value = "";
 }
 
 function keyEnter(){
 	document.addEventListener("keydown", (event) => {
 		if (event.keyCode == 13) {
-			userInput.push(" " + document.getElementById('i').value);
+			if (document.getElementById('i').innerHTML !== ""){
+				document.getElementById('i').blur();
+				send();
+			}
 		}
 	});
 }
