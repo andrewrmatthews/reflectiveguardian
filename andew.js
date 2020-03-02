286fe26bf91ca37859ab5ff6ff07537de8d9f7cb
andrewrmatthews <60239972+andrewrmatthews@users.noreply.github.com>
2020-03-02 15:09:29 -0800

    Update andew.js

--- andew.js
+++ andew.js
@@ -5,15 +5,16 @@ function send(){
 	document.getElementById("i").value = "";
 }
 
-function keyEnter(){
-	document.addEventListener("keydown", (event) => {
-		if (event.keyCode == 13) {
-			if (document.getElementById('i').innerHTML !== ""){
-				document.getElementById('i').blur();
-				send();
-			}
-		}
-	});
-}
+// function keyEnter(){
+// 	document.addEventListener("keydown", (event) => {
+// 		if (event.keyCode == 13) {
+// 			if (document.getElementById('i').innerHTML !== ""){
+// 				document.getElementById('i').blur();
+// 				event.preventDefault();
+// 				send();
+// 			}
+// 		}
+// 	});
+// }
 
 var userInput = [];
