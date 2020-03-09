a4f11fd19678a3c35eeb1e37ecd4e56680d7df01
andrewrmatthews <60239972+andrewrmatthews@users.noreply.github.com>
2020-03-09 15:10:41 -0700

    Update andew.js

--- andew.js
+++ andew.js
@@ -1,20 +1,21 @@
 function send(){
-	var pullInput = " " + document.getElementById('i').value;
+	var pullInput = document.getElementById('i').value + "<br>";
 	userInput.push(pullInput);
 	document.getElementById("chatbox").innerHTML = userInput;
 	document.getElementById("i").value = "";
 }
 
-// function keyEnter(){
-// 	document.addEventListener("keydown", (event) => {
-// 		if (event.keyCode == 13) {
-// 			if (document.getElementById('i').innerHTML !== ""){
-// 				document.getElementById('i').blur();
-// 				event.preventDefault();
-// 				send();
-// 			}
-// 		}
-// 	});
-// }
+ function keyEnter(){
+ 	document.addEventListener("keydown", (event) => {
+ 		if (event.keyCode == 13) {
+ 			if (document.getElementById('i').innerHTML !== ""){
+ 				document.getElementById('i').blur();
+ 				event.preventDefault();
+ 				send();
+ 			}
+ 		}
+ 	});
+}
+
 
 var userInput = [];
